13035687ae2d95496c19d8c77c087bfd8f93234f
timur5050 <stukantimur811@gmail.com>
2025-08-01 18:48:11 +0300

    added switching altsettings and also switched to second interface

--- wdriver.c
+++ wdriver.c
@@ -10,7 +10,7 @@
 
 
 static struct usb_device_id webcam_logger_table[] = {
-    { USB_DEVICE_AND_INTERFACE_INFO(VENDOR_ID, DEVICE_ID, USB_CLASS_VIDEO, 1, 0) },
+    { USB_DEVICE_AND_INTERFACE_INFO(VENDOR_ID, DEVICE_ID, USB_CLASS_VIDEO, 2, 0) },
     {}
 };
 MODULE_DEVICE_TABLE(usb, webcam_logger_table);
@@ -18,7 +18,7 @@ MODULE_DEVICE_TABLE(usb, webcam_logger_table);
 struct webcam_logger {
     struct usb_device *udev;
     struct usb_interface *interface;
-    struct usb_endpoint_descriptor *ep_desc;
+    struct usb_host_endpoint *ep;
     unsigned char *data;
     struct urb *urb;
 };
@@ -57,7 +57,39 @@ static int webcam_logger_probe(struct usb_interface *interface, const struct usb
                     le16_to_cpu(temp_desc->wMaxPacketSize));
         }
     }
+    
+    struct usb_host_interface *cur_alt = interface->cur_altsetting;
+    int alt_settings = 3;
 
+    retval = usb_set_interface(udev, cur_alt->desc.bInterfaceNumber, alt_settings);
+    if (retval) {
+        pr_info(DRV_NAME ": not allowed to switch altsetting: %d\n", retval);
+    }
+    
+    pr_info(DRV_NAME ": curr setting review and choosing endpoint:\n");
+    cur_alt = interface->cur_altsetting;
+
+    for(int i = 0; i < cur_alt->desc.bInterfaceNumber; i++)
+    {
+        struct usb_endpoint_descriptor *temp_desc = &cur_alt->endpoint[i].desc;
+        pr_info(DRV_NAME " : i = %d, type: 0x%x, address: 0x%02x, maxsize: %d\n",
+            i,
+            temp_desc->bmAttributes,
+            temp_desc->bEndpointAddress,
+            le16_to_cpu(temp_desc->wMaxPacketSize));
+        if(usb_endpoint_xfer_isoc(temp_desc))
+        {
+            dev->ep = &cur_alt->endpoint[i];
+            pr_info(DRV_NAME " : found isoc ep, ep num : %d\n", usb_endpoint_num(&dev->ep->desc));
+            if (usb_endpoint_dir_in(&dev->ep->desc)) {
+                pr_info(DRV_NAME ": that endpoint is IN type\n");
+            } else if (usb_endpoint_dir_out(&dev->ep->desc)) {
+                pr_info(DRV_NAME ": that endpoint is OUT type\n");
+            } else {
+                pr_info(DRV_NAME ": error checking in/out type\n");
+            }
+        }
+    }
 
     return retval;
 }
