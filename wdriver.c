deeece02155070b17ed9ca8f922b5822505ec630
timur5050 <stukantimur811@gmail.com>
2025-08-01 17:51:20 +0300

    added lopp for checking all available altsettings from FIRST interface

--- wdriver.c
+++ wdriver.c
@@ -34,6 +34,31 @@ static int webcam_logger_probe(struct usb_interface *interface, const struct usb
             udev->manufacturer ? udev->manufacturer : "Unknown",
             udev->product ? udev->product : "Unknown");
     
+    dev = kzalloc(sizeof(*dev), GFP_KERNEL);
+    if(!dev)
+    {
+        return -ENOMEM;
+    }
+
+    dev->udev = usb_get_dev(udev);
+    dev->interface = interface;
+
+    for(i = 0; i < dev->interace->num_altsetting; i++)
+    {
+        pr_info(DRV_NAME " : altset num : %d\n", i);
+        struct usb_host_interface *temp = &dev->interface->altsetting[i];
+        for(int j = 0; j < temp->desc.bNumEndpoints; j++)
+        {
+            struct usb_endpoint_descriptor *temp_desc = &temp->endpoint[j].desc;
+            pr_info(DRV_NAME " : j = %d, type: 0x%x, address: 0x%02x, maxsize: %d\n",
+                    j,
+                    temp_desc->bmAttributes,
+                    temp_desc->bEndpointAddress,
+                    le16_to_cpu(temp_desc->wMaxPacketSize));
+        }
+    }
+
+
     return retval;
 }
 
