511fa1e6b6d3442b40dcc399126b2c1b1b6bdea1
timur5050 <stukantimur811@gmail.com>
2025-08-02 13:41:58 +0300

    small fixes and also added goto for probe

--- wdriver.c
+++ wdriver.c
@@ -37,7 +37,7 @@ static void urb_complete_handler(struct urb *urb)
 {
     struct webcam_logger *dev = urb->context;
 
-    for(int i = 0; i < urb->number_of_packets;; i++)
+    for(int i = 0; i < urb->number_of_packets; i++)
     {
         struct usb_iso_packet_descriptor *desc = &urb->iso_frame_desc[i];
 
@@ -91,7 +91,7 @@ int setup_iso_urbs(struct webcam_logger *dev)
         urb = usb_alloc_urb(NUM_PACKETS_PER_URB, GFP_KERNEL);
         if(!urb)
         {
-            kree(buf);
+            kfree(buf);
             goto error;
         }
 
@@ -119,7 +119,7 @@ int setup_iso_urbs(struct webcam_logger *dev)
         dev->urbs[i].buffer = buf;
 
         // 6. sent urb to host-contrller
-        ret = urb_submit_urb(urb, GFP_KERNEL);
+        ret = usb_submit_urb(urb, GFP_KERNEL);
         if(ret)
         {
             pr_err(DRV_NAME, " : submit URB %d failed: %d\n", i, ret);
@@ -148,93 +148,81 @@ static int webcam_logger_probe(struct usb_interface *interface, const struct usb
     struct usb_device *udev = interface_to_usbdev(interface);
     struct webcam_logger *dev;
     int retval = 0;
-    int i;
 
     pr_info(DRV_NAME " probe called for device : %s:%s\n",
             udev->manufacturer ? udev->manufacturer : "Unknown",
             udev->product ? udev->product : "Unknown");
     
     dev = kzalloc(sizeof(*dev), GFP_KERNEL);
-    if(!dev)
-    {
+    if (!dev)
         return -ENOMEM;
-    }
 
     dev->udev = usb_get_dev(udev);
     dev->interface = interface;
 
-    for(i = 0; i < dev->interface->num_altsetting; i++)
+    for (int i = 0; i < dev->interface->num_altsetting; i++) 
     {
-        pr_info(DRV_NAME " : altset num : %d\n", i);
-        struct usb_host_interface *temp = &dev->interface->altsetting[i];
-        for(int j = 0; j < temp->desc.bNumEndpoints; j++)
+        struct usb_host_interface *alt = &dev->interface->altsetting[i];
+        pr_info(DRV_NAME ": altset num : %d\n", i);
+        for (int j = 0; j < alt->desc.bNumEndpoints; j++)
         {
-            struct usb_endpoint_descriptor *temp_desc = &temp->endpoint[j].desc;
-            pr_info(DRV_NAME " : j = %d, type: 0x%x, address: 0x%02x, maxsize: %d\n",
-                    j,
-                    temp_desc->bmAttributes,
-                    temp_desc->bEndpointAddress,
-                    le16_to_cpu(temp_desc->wMaxPacketSize));
+            struct usb_endpoint_descriptor *d = &alt->endpoint[j].desc;
+            pr_info(DRV_NAME ": ep %d, type: 0x%x, addr: 0x%02x, maxsize: %d\n",
+                    j, d->bmAttributes, d->bEndpointAddress, le16_to_cpu(d->wMaxPacketSize));
         }
     }
-    
-    struct usb_host_interface *cur_alt = interface->cur_altsetting;
+
     int alt_settings = 3;
-
-    retval = usb_set_interface(udev, cur_alt->desc.bInterfaceNumber, alt_settings);
-    if (retval) {
-        pr_info(DRV_NAME ": not allowed to switch altsetting: %d\n", retval);
-    }
-    
-    pr_info(DRV_NAME ": curr setting review and choosing endpoint:\n");
-    cur_alt = interface->cur_altsetting;
-
-    for(int i = 0; i < cur_alt->desc.bInterfaceNumber; i++)
+    retval = usb_set_interface(udev, interface->cur_altsetting->desc.bInterfaceNumber, alt_settings);
+    if (retval) 
     {
-        struct usb_endpoint_descriptor *temp_desc = &cur_alt->endpoint[i].desc;
-        pr_info(DRV_NAME " : i = %d, type: 0x%x, address: 0x%02x, maxsize: %d\n",
-            i,
-            temp_desc->bmAttributes,
-            temp_desc->bEndpointAddress,
-            le16_to_cpu(temp_desc->wMaxPacketSize));
-        if(usb_endpoint_xfer_isoc(temp_desc))
+        pr_info(DRV_NAME ": usb_set_interface failed: %d\n", retval);
+        goto fail_free;
+    }
+
+    struct usb_host_interface *cur_alt = interface->cur_altsetting;
+    for (int i = 0; i < cur_alt->desc.bNumEndpoints; i++)
+    {
+        struct usb_endpoint_descriptor *desc = &cur_alt->endpoint[i].desc;
+        if (usb_endpoint_xfer_isoc(desc))
         {
             dev->ep = &cur_alt->endpoint[i];
-            pr_info(DRV_NAME " : found isoc ep, type: 0x%x, address: 0x%02x, maxsize: %d\n",
-                dev->ep->desc.bmAttributes,
-                dev->ep->desc.bEndpointAddress,
-                le16_to_cpu(dev->ep->desc.wMaxPacketSize));
-            if (usb_endpoint_dir_in(&dev->ep->desc)) {
-                pr_info(DRV_NAME ": that endpoint is IN type\n");
-            } else if (usb_endpoint_dir_out(&dev->ep->desc)) {
-                pr_info(DRV_NAME ": that endpoint is OUT type\n");
-            } else {
-                pr_info(DRV_NAME ": error checking in/out type\n");
-            }
+            pr_info(DRV_NAME ": found isoc ep: addr=0x%02x size=%d\n",
+                    desc->bEndpointAddress,
+                    le16_to_cpu(desc->wMaxPacketSize));
+            break;
         }
     }
 
-    if(!dev->ep)
-    {
-        pr_err(DRV_NAME " could not find interrupt IN endpoint\n");
+    if (!dev->ep) {
+        pr_err(DRV_NAME ": no isoc endpoint found\n");
         retval = -ENODEV;
+        goto fail_free;
     }
 
-    dev->data = kmalloc(usb_endpoint_maxp(dev->ep->desc));
-    if(!dev->data)
-    {
+    dev->data = kmalloc(usb_endpoint_maxp(dev->ep->desc), GFP_KERNEL);
+    if (!dev->data) {
         retval = -ENOMEM;
+        goto fail_free;
     }
 
-    dev->urb = usb_alloc_urb(0, GFP_KERNEL);
-    if(!dev->urb)
-    {
-        retval = -ENOMEM;
+    retval = setup_iso_urbs(dev);
+    if (retval) {
+        pr_err(DRV_NAME ": failed to setup urbs\n");
+        goto fail_data;
     }
-    
 
+    usb_set_intfdata(interface, dev);
+    return 0;
+
+fail_data:
+    kfree(dev->data);
+fail_free:
+    usb_put_dev(dev->udev);
+    kfree(dev);
     return retval;
 }
+
 
 static void webcam_logger_disconnect(struct usb_interface *interface)
 {
